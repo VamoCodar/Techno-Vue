c540cc794acb901377666a5bc4cdd0cb6c2509de
VamoCodar <senhoraroldo@gmail.com>
2021-07-04 21:33:16 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const vm = new Vue({
+    el: "#app",
+    data: {
+        produtos: [],
+        produto: false,
+    },
+    filters: {
+        numeroPreco(valor) {
+            return valor.toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL"
+            })
+        }
+    },
+    methods: {
+        fetchProdutos() {
+            fetch("./api/produtos.json")
+                .then(r => r.json())
+                .then(json => this.produtos = json)
+        },
+        fetchProduto(id) {
+            fetch(`./api/produtos/${id}/dados.json`)
+                .then(r => r.json())
+                .then(json => this.produto = json)
+        },
+        abrirModal(id) {
+            this.fetchProduto(id)
+            window.scrollTo({
+                top:0,
+                behavior:"smooth"
+            })
+        },
+        fecharModal({
+            target,
+            currentTarget
+        }) {
+            if (target === currentTarget)
+                this.produto = false
+        }
+    },
+
+
+    created() {
+        this.fetchProdutos()
+    }
+})
